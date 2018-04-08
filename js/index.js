9395835a3fac8abbe29de1f77693ac8f88f8fc42
armsarms <armsarms@sina.com>
2018-04-08 11:26:07 +0800

    V2

--- js/index.js
+++ js/index.js
@@ -1,69 +1,138 @@
 $(function () {
+    var i = 1;
     $('.table-left ul.nav li').click(function () {
         $('.table-left ul.nav li').removeClass('active');
         $(this).addClass('active');
         var tableName = $(this).find('a').attr('data-table-name');
         $('.table-right .table-main').removeClass('table-show');
         $('.' + tableName).addClass('table-show');
+        if (tableName == 'table-report') {
+            $('#newForm :input').not(':button').val('').removeAttr('checked');
+            $('.new-group').eq(0).siblings('.new-group').remove();
+            $('.redWarning').removeClass('redWarning')
+        }
     })
 
     $('.table-report button').click(function () {
+        var form = $("#newForm").serializeArray();
         switch ($(this).attr('id')) {
-            case 'btnPrimary':
-                var form = $("#newForm").serializeArray();
-                // var reportWork = $('#reportWeek .form-control').serializeArray();
-                // var strWork = '';
-                // for (var i = 0; i < reportWork.length; i++) {
-                    // strWork = strWork + (i + 1 + '.' + reportWork[i].value + '<br>');
-                // }
-                $(".table-draft tbody").append("<tr><td>" + form[3].value + "</td><td>" + form[0].value + "</td><td>" + form[1].value + ' ~ ' + form[2].value + "</td><td>" + loopTable('reportWeek') + "</td><td>" + loopTable('reportNextWeek') + "</td><td>" + loopTable('reportConsort') + "</td><td><button type='button' class='btn btn-primary'>提交</button> <button type='button' class='btn btn-success'>修改</button></td></tr>");
-                $('.success-modal').show().fadeOut(1000).text('提交成功');
-                $("[data-table-name='table-draft']").parents('li').click();
-                break;
             case 'btnSuccess':
-                $('.success-modal').show().fadeOut(1000).text('保存成功');
+                if (form[0].value != '' && form[1].value != '') {
+                    for (var i = 0; i < $('.new-group').length; i++) {
+                        $(".table-draft tbody").append("<tr><td><input type='checkbox'></td><td>" + form[0].value + "</td><td>" + form[1].value + "</td><td>" + $(".new-group .reportTitle").eq(i).val().replace(/\n|\r\n/g, '<br/>') + "</td><td>" + $(".new-group .reportWeek").eq(i).val().replace(/\n|\r\n/g, '<br/>') + "</td><td>" + $(".new-group .reportNextWeek").eq(i).val().replace(/\n|\r\n/g, '<br/>') + "</td><td>" + $(".new-group .reportConsort").eq(i).val().replace(/\n|\r\n/g, '<br/>') + "</td></tr>");
+                        // form[4+4*i].value.replace(/\n|\r\n/g,'<br/>'),输入栏导入换行
+                    }
+                    if ($(".table-draft tr[data-change='inChange']").length == 1) {
+                        $(".table-draft tr[data-change='inChange']").remove();
+                    }
+                    $('.success-modal').show().fadeOut(2000).text('保存成功');
+                    $("[data-table-name='table-draft']").parents('li').click();
+                } else {
+                    if (form[0].value == '') {
+                        $("#reportTime").addClass('redWarning');
+                    }
+                    if (form[1].value == '') {
+                        $("#reportGroup").addClass('redWarning');
+                    }
+                    $('.success-modal').show().fadeOut(2000).text('保存失败');
+                }
+                break;
+            case 'btnPrimary':
+                if (form[0].value != '' && form[1].value != '' && $(".new-group .reportTitle").val() != '' &&  $(".new-group .reportWeek").val() != '' &&  $(".new-group .reportNextWeek").val() != '' ){
+                    $('.success-modal').show().fadeOut(2000).text('提交成功');
+                } else {
+                    $('.success-modal').show().fadeOut(2000).text('提交失败');
+                }
                 break;
         }
     })
 
     $('.increase-btn').click(function () {
-        //     var i = $(this).parents('.col-sm-5').find('input').length + 1;
-        //     $(this).before("<div class='increase-input'><input class='form-control' id='reportWeek' type='text' name='reportWeek' style='margin-top:15px;'placeholder='" + i + ".'><button type='button' class='btn btn-primary btn-sm' id='decreaseBtn'><span class='glyphicon glyphicon-minus'></span></button></div>");
-        increase($(this));
+        var html = "<div class='new-group' data-number=" + i++ + "><hr><input type='checkbox' class='new-group-check'><div class='form-group report-title'><label class='col-sm-2 control-label'>周报标题：</label><div class='col-sm-5'><input class='form-control reportTitle' type='text' name='title'></div></div><div class='form-group report-week'><label class='col-sm-2 control-label'>本周重点工作：</label><div class='col-sm-5'><textarea class='form-control reportWeek' rows='5' name='reportWeek'></textarea></div></div><div class='form-group report-next-week'><label class='col-sm-2 control-label'>下周计划：</label><div class='col-sm-5'><textarea class='form-control reportNextWeek' rows='3' id='reportNextWeek'></textarea></div></div><div class='form-group report-consort'><label class='col-sm-2 control-label'>需领导协调事项：</label><div class='col-sm-5'><textarea class='form-control reportConsort' rows='3' name='reportConsort'></textarea></div></div></div>"
+        $(html).insertBefore(".crease-btn");
+        // $(".new-group").last().clone().insertBefore(".crease-btn");
+        // $(".new-group").last().find('input,textarea').each(function () {
+        //     $(this).val('');
+        // });
     })
 
-    $('.table-report').on("click", ".decrease-btn", function () {
-        // var input = $(this).parents('.increase-input');
-        // var inputP = input.siblings('.increase-input')
-        // input.remove();
-        // //    console.log(input.index());
-        // for (var i = 0; i < inputP.length; i++) {
-        //     inputP.eq(i).find('input').attr('placeholder', i + 2)
+    $('.decrease-btn').click(function () {
+        var $check = $('.new-group .new-group-check:checked');
+        if ($check.length > 0) {
+            $check.parents('.new-group').remove();
+        } else if ($check.length == 0) {
+            $('.success-modal').show().fadeOut(2000).text('请至少选中一个选框');
+        }
+        // if ($(".new-group").length > 1) {
+        //     $(".new-group").last().remove();
         // }
-        decrease($(this));
+    })
+
+    $('#draftDel').click(function () {
+        var $check = $('.table-draft .table input:checked');
+        if ($check.length > 0) {
+            $check.parents('tr').remove();
+        } else if ($check.length == 0) {
+            $('.success-modal').show().fadeOut(2000).text('请至少选中一个选框');
+        }
+        // if ($(".new-group").length > 1) {
+        //     $(".new-group").last().remove();
+        // }
+    })
+
+    $('#draftChange').click(function () {
+        var $check = $('.table-draft .table-hover input:checked');
+        //if()，如果new-group.length>1,删除，否则，加一
+        if ($check.length == 1) {
+            $check.parents('tr').attr('data-change', 'inChange')
+            $check.parents('tr')
+            $("[data-table-name='table-report']").click();
+            if ($('.new-group').length > 1) {
+                $('.new-group').eq(0).siblings('.new-group').remove();
+            } else if ($('.new-group').length < 1) {
+                $('.increase-btn').click();
+            }
+            $check.parents('tr').find('td').each(function (i) {
+                switch (i) {
+                    case 1:
+                        $('#reportTime').val($(this).text());
+                        break;
+                    case 2:
+                        $('#reportGroup').val($(this).text());
+                        break;
+                    case 3:
+                        $('.reportTitle').val($(this).text());
+                        break;
+                    case 4:
+                        $('.reportWeek').val($(this).html().replace(/<br>/g, '\n'));
+                        // $('.reportWeek').val($(this).html().replace(/\n|\r\n/g,'<br/>'));
+                        break;
+                    case 5:
+                        $('.reportNextWeek').val($(this).html().replace(/<br>/g, '\n'));
+                        break;
+                    case 6:
+                        $('.reportConsort').val($(this).html().replace(/<br>/g, '\n'));
+                        break;
+                }
+            })
+        } else if ($check.length > 1) {
+            $('.success-modal').show().fadeOut(2000).text('只能同时编辑一条周报');
+        } else {
+            $('.success-modal').show().fadeOut(2000).text('请选中一个选框');
+        }
+    })
+
+    $("#reportTime,#reportGroup").click(function () {
+        $(this).removeClass('redWarning');
+    })
+
+    $('.table-receive tr').click(function () {
+        $('#myModal').modal('show').find('.modal-title').text($(this).find('td').text())
+    })
+
+    $('.modal').on('hidden.bs.modal', function () {
+        if ($('.modal.in').size() >= 1) {
+            $('body').addClass('modal-open')
+        }
     })
 })
-
-function increase(e) {
-    var i = e.parents('.col-sm-5').find('input').length + 1;
-    e.before("<div class='increase-input'><input class='form-control' type='text' name='reportWeek' style='margin-top:15px;'placeholder='" + i + ".'><button type='button' class='btn btn-primary btn-sm decrease-btn'><span class='glyphicon glyphicon-minus'></span></button></div>");
-}
-
-function decrease(e) {
-    var input = e.parents('.increase-input');
-    var inputP = input.siblings('.increase-input')
-    input.remove();
-    //    console.log(input.index());
-    for (var i = 0; i < inputP.length; i++) {
-        inputP.eq(i).find('input').attr('placeholder', i + 2)
-    }
-}
-
-function loopTable(id) {
-    var form = $('#'+id+' .form-control').serializeArray();
-    var strWork = '';
-    for (var i = 0; i < form.length; i++) {
-        strWork = strWork + (i + 1 + '. ' + form[i].value + '<br>');
-    }
-    return strWork
-}
